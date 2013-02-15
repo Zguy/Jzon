40348b341f8cd21c48954c05c44a4b8acbdc87e8
Johannes Häggqvist <johanneshagg@gmail.com>
2013-02-15 18:20:32 +0100

    Updated copyright year

--- Jzon.cpp
+++ Jzon.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012 Johannes Häggqvist
+Copyright (c) 2013 Johannes Häggqvist
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
