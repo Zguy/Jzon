2e38a982a412087970776378006ed3513aa307fa
Lex Robinson <lexi@lexi.org.uk>
2013-11-23 20:59:54 +0000

    Added fail tests

--- test/test.sh
+++ test/test.sh
@@ -9,17 +9,37 @@ run_prog() {
 	$prog "spec/"$1$2".json" 2>$outfile || success=false
 }
 
+passes=0
+fails=0
+
 run_success() {
 	echo "Running pass test ${1}"
 	run_prog "pass" $1
 	if $success; then
-		echo -e $PASS
+		((passes++))
+		echo -e $PASS" JSON parsed successfully!"
 	else
+		((fails++))
 		echo -en $FAIL" "
 		cat $outfile
 		failure=true
 	fi
 }
+
+run_failure() {
+	echo "Running fail test ${1}"
+	run_prog "fail" $1
+	if $success; then
+		((fails++))
+		echo -e $FAIL" JSON parsed successfully!"
+	else
+		((passes++))
+		echo -en $PASS" "
+		cat $outfile
+		failure=true
+	fi
+}
+
 touch $outfile
 
 failure=false
@@ -28,8 +48,14 @@ for i in $(seq 1 3); do
 	run_success $i
 done
 
+for i in $(seq 1 33); do
+	run_failure $i
+done
 
 rm $outfile
+
+echo
+echo "Tests completed with "$passes" passes and "$fails" failures"
 
 if [ $failure ]; then
 	exit 1
