90f2c3e5da5b72ec5f2dd36cd936f5f2d47425ab
Lex Robinson <lexi@lexi.org.uk>
2013-11-23 20:44:23 +0000

    Added initial makefile & tests

---
+++ test/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+prog=$1
+outfile="test.out"
+FAIL='\E[1;31m'"FAIL"'\E[0m'
+PASS='\E[1;32m'"PASS"'\E[0m'
+
+run_prog() {
+	success=true
+	$prog "spec/"$1$2".json" 2>$outfile || success=false
+}
+
+run_success() {
+	echo "Running pass test ${1}"
+	run_prog "pass" $1
+	if $success; then
+		echo -e $PASS
+	else
+		echo -en $FAIL" "
+		cat $outfile
+		failure=true
+	fi
+}
+touch $outfile
+
+failure=false
+
+run_success 1;
+run_success 2;
+run_success 3;
+
+
+rm $outfile
+
+if [ $failure ]; then
+	exit 1
+else
+	exit 0
+fi
+
+
